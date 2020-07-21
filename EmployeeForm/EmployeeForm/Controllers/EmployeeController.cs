c74b354d19b99353acd122340fe36dadb1a73368
Dhrumil Gohil <57474739+DhrumilGohil@users.noreply.github.com>
2020-07-21 23:23:11 +0530

    Latest...

---
+++ EmployeeForm/EmployeeForm/Controllers/EmployeeController.cs
@@ -0,0 +1,288 @@
+﻿using EmployeeForm.Models;
+using EmployeeForm.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using Microsoft.Ajax.Utilities;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices;
+using PagedList;
+using PagedList.Mvc;
+using System.Web.UI;
+
+namespace EmployeeForm.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+         public ActionResult ShowAllEmployee(int? page,string Search)
+         {
+             EmpRepository EmpRepo = new EmpRepository();
+             ModelState.Clear();
+            List<Employee> test = new List<Employee>();
+            test = EmpRepo.GetAllEmployee(0).ToList();
+            if (Search!= null){ 
+                test = test.Where(x => x.FirstName.Contains(Search) ||  x.LastName.Contains(Search)).ToList(); 
+            }
+            return View(test.ToPagedList(page ?? 1,3));
+         }
+
+
+        // GET: Employee/Create
+        public ActionResult AddEmp()
+        {            
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult AddEmp(Employee Emp)
+        {
+            int now = DateTime.Now.Year;
+            int years = Emp.DateOfBirth.Year;
+            if ((now - years) < 18)
+            {
+                ViewBag.Message = "You are not Eligible to apply..";
+            }
+            else if (Emp.DateOfBirth > Emp.Dateofjoin)
+            {
+                ViewBag.ErrorMessage = "Conflicts in Birthdate and joining Date";
+            }
+            else
+            {
+                EmpRepository Emprepo = new EmpRepository();
+                if (Emprepo.AddEmployee(Emp))
+                {
+                    return RedirectToAction("ShowAllEmployee");
+                }
+                else
+                {
+                    return RedirectToAction("Error");
+                }
+
+            }
+            return View();
+
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult EditEmp(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            List<Employee> EditEmployee = new List<Employee>();
+            EditEmployee = Emprepo.GetAllEmployee(id);
+            return View(EditEmployee);
+        }                      
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult EditEmp(Employee Emp)
+        {
+            int now = DateTime.Now.Year;
+            int years = Emp.DateOfBirth.Year;
+            if ((now - years) < 18)
+            {
+                ViewBag.Message = "You are not Eligible to apply..";
+            }
+            else if (Emp.DateOfBirth > Emp.Dateofjoin)
+            {
+                ViewBag.ErrorMessage = "Conflicts in Birthdate and joining Date";
+            }
+            else
+            {
+                EmpRepository Emprepo = new EmpRepository();
+                if (Emprepo.UpdateEmp(Emp))
+                {
+                    return RedirectToAction("ShowAllEmployee");
+                }
+                else
+                {
+                    return RedirectToAction("Error");
+                }
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetAllDistrict()
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            List<District> Districts = Emprepo.GetAllDistrict().ToList();            
+            return Json(Districts, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult GetAllTalukas(int Empid)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            List<Taluka> Talukas = Emprepo.GetAllTaluka(Empid);            
+            return Json(Talukas, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult GetAllAgencies()
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            List<string> Agencies = Emprepo.GetAllAgency();
+            return Json(Agencies,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DetailS(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            ModelState.Clear();
+            return View(Emprepo.GetAllEmployee(id));
+        }
+
+        public ActionResult EmpActive(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("ISACTIVE", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmployeeID", id);
+            com.Parameters.AddWithValue("@isActive", 0);
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return RedirectToAction("ShowAllEmployee");
+            }
+            else
+            { 
+                return RedirectToAction("Error");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult search(string Search)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            List<Employee> test = new List<Employee>();
+            test = Emprepo.GetAllEmployee();
+            return View(test.Where(x=> x.FirstName.Contains(Search)).ToList());
+        }
+
+        public ActionResult MehkamDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Cadres()
+        {
+            return View();
+        }
+
+        public ActionResult Agency()
+        {
+            return View();
+        }          
+
+        public ActionResult EmployeeJobDetails()
+        {
+            return View();
+        }
+
+        public ActionResult Taluka()
+        {
+            return View();
+        }
+        public ActionResult District()
+        {
+            return View();
+        }
+
+        public ActionResult Exam()
+        {
+            return View();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        }
+
+        public ActionResult EmployeeExam()
+        {
+            return View();
+        }
+
+        public ActionResult Office()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Designation()
+        {
+            return View();
+        }
+        public ActionResult ShowListDistrict(int? page)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            return View(Emprepo.AllDistricsDetail().ToPagedList(page ?? 1, 10));
+        }
+        public ActionResult ShowListTaluka(int? page)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            return View(Emprepo.AllTalukasDetail().ToPagedList(page ?? 1,10));
+        }
+    }
+}
+
+        // GET: Employee/Delete/5
+      /*  public ActionResult DeleteEmp(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            Emprepo.deleteEmployee(id);
+
+            return RedirectToAction("ShowAllEmployee");
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult DeleteEmp()
+        {
+            return RedirectToAction("ShowAllEmployee");
+
+        }
+       
+        public ActionResult EmpActive(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("ISACTIVE", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmployeeID", id);
+            com.Parameters.AddWithValue("@isActive", 0);
+            com.ExecuteNonQuery();
+
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if(i > 1)
+            {
+                return RedirectToAction("ShowAllEmployee");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+            
+        }
+
+        public ActionResult Detail(int id)
+        {
+            EmpRepository Emprepo = new EmpRepository();
+            ModelState.Clear();
+            return View(Emprepo.GetAllEmployee().Find(emp => emp.EmployeeID == id));
+        }
+
+
+    }
+}*/
---
+++ EmployeeForm/EmployeeForm/Repository/EmployeeRepo.cs
@@ -0,0 +1,330 @@
+﻿using EmployeeForm.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Web.WebSockets;
+
+namespace EmployeeForm.Repository
+{
+    public class EmpRepository
+    {
+        public List<Employee> GetAllEmployee([Optional]int EmployeeID)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            List<Employee> EmpList = new List<Employee>();
+            SqlCommand com = new SqlCommand("GetEmployees", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmployeeID", EmployeeID);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmpList.Add(
+
+                    new Employee()
+                    {
+                        EmployeeID = Convert.ToInt32(dr["EmployeeID"]),
+                        FirstName = Convert.ToString(dr["FirstName"]),
+                        MiddleName = Convert.ToString(dr["MiddleName"]),
+                        LastName = Convert.ToString(dr["LastName"]),
+                        Gender = (dr["Gender"].ToString()),
+                        Email = Convert.ToString(dr["Email"]),
+                        PresentAddress = Convert.ToString(dr["PresentAddress"]),
+                        PresentDistrictID = Convert.ToInt32(dr["PresentDistrictID"]),
+                        PresentTalukaID = Convert.ToInt32(dr["PresentTalukaID"]),
+                        PresentPINCODE = Convert.ToInt32(dr["PresentPINCODE"]),
+                        NativeAddress = Convert.ToString(dr["NativeAddress"]),
+                        NativeDistrictID = Convert.ToInt32(dr["NativeDistrictID"]),
+                        NativeTalukaID = Convert.ToInt32(dr["NativeTalukaID"]),
+                        NativePINCODE = Convert.ToInt32(dr["NativePINCODE"]),
+                        PhoneNumber = Convert.ToString(dr["PhoneNumber"]),
+                        AlternatePhoneNumber = Convert.ToString(dr["AlternatePhoneNumber"]),
+                        EmergencyContactNumber = Convert.ToString(dr["EmergencyContactNumber"]),
+                        EmergencyContactName = Convert.ToString(dr["EmergencyContactName"]),
+                        DateOfBirth = Convert.ToDateTime(dr["DateOfBirth"]),
+                        Dateofjoin = Convert.ToDateTime(dr["DateOfJoin"]),
+                        DateofRetirement = Convert.ToDateTime(dr["DateofRetirement"]),
+                        DistrictJoiningDate = Convert.ToDateTime(dr["DistrictJoiningDate"]),
+                        EmployeeEntryinDistrict = Convert.ToString(dr["EmployeeEntryinDistrict"]),
+                        EmployeeType = Convert.ToString(dr["EmployeeType"]),
+                        AgencyID = Convert.ToInt32(dr["AgencyID"]),
+                        CreatedBy = Convert.ToInt32(dr["CreatedBy"]),
+                        CreatedDate = Convert.ToDateTime(dr["CreatedDate"]),
+                        UpdatedBy = Convert.ToInt32(dr["UpdatedBy"]),
+                        UpdatedDate = Convert.ToDateTime(dr["UpdatedDate"]),
+                        isActive = Convert.ToBoolean(dr["isActive"]),
+                    }
+
+                    );
+            }
+
+            return EmpList;
+
+
+        }
+
+        public bool AddEmployee(Employee obj)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("AddNewEmployee", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@FirstName", obj.FirstName);
+            com.Parameters.AddWithValue("@LastName", obj.LastName);
+            com.Parameters.AddWithValue("@MiddleName", obj.MiddleName);
+            com.Parameters.AddWithValue("@Gender", Convert.ToString(obj.Gender));
+            com.Parameters.AddWithValue("@Email", obj.Email);
+            com.Parameters.AddWithValue("@PresentAddress", obj.PresentAddress);
+            com.Parameters.AddWithValue("@PresentDistrictID", obj.PresentDistrictID);
+            com.Parameters.AddWithValue("@PresentTalukaID", obj.PresentTalukaID);
+            com.Parameters.AddWithValue("@PresentPinCode", obj.PresentPINCODE);
+            com.Parameters.AddWithValue("@NativeAddress", obj.NativeAddress);
+            com.Parameters.AddWithValue("@NativeDistrictID", obj.NativeDistrictID);
+            com.Parameters.AddWithValue("@NativeTalukaID", obj.NativeTalukaID);
+            com.Parameters.AddWithValue("@NativePinCode", obj.NativePINCODE);
+            com.Parameters.AddWithValue("@PhoneNumber", obj.PhoneNumber);
+            com.Parameters.AddWithValue("@AlternatePhoneNumber", obj.AlternatePhoneNumber);
+            com.Parameters.AddWithValue("@EmergencyContactNumber", obj.EmergencyContactNumber);
+            com.Parameters.AddWithValue("@EmergencyContactName", obj.EmergencyContactName);
+            com.Parameters.AddWithValue("@DateOfBirth", obj.DateOfBirth);
+            com.Parameters.AddWithValue("@Dateofjoin", obj.Dateofjoin);
+            com.Parameters.AddWithValue("@DateofRetirement", obj.DateofRetirement);
+            com.Parameters.AddWithValue("@DistrictJoiningDate", obj.DistrictJoiningDate);
+            com.Parameters.AddWithValue("@EmployeeEntryinDistrict", Convert.ToString(obj.EmployeeEntryinDistrict));
+            com.Parameters.AddWithValue("@EmployeeType", obj.EmployeeType);
+            com.Parameters.AddWithValue("@AgencyID", obj.AgencyID);
+            com.Parameters.AddWithValue("@CreatedBy", obj.CreatedBy);
+            com.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+            com.Parameters.AddWithValue("@UpdatedBy", obj.UpdatedBy);
+            com.Parameters.AddWithValue("@UpdatedDate", DateTime.Now);
+            com.Parameters.AddWithValue("@isActive", 1);
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateEmp(Employee obj)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("UpdateEmployee", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmployeeID", obj.EmployeeID);
+            com.Parameters.AddWithValue("@FirstName", obj.FirstName);
+            com.Parameters.AddWithValue("@LastName", obj.LastName);
+            com.Parameters.AddWithValue("@MiddleName", obj.MiddleName);
+            com.Parameters.AddWithValue("@Gender", Convert.ToString(obj.Gender));
+            com.Parameters.AddWithValue("@Email", obj.Email);
+            com.Parameters.AddWithValue("@PresentAddress", obj.PresentAddress);
+            com.Parameters.AddWithValue("@PresentDistrictID", obj.PresentDistrictID);
+            com.Parameters.AddWithValue("@PresentTalukaID", obj.PresentTalukaID);
+            com.Parameters.AddWithValue("@PresentPinCode", obj.PresentPINCODE);
+            com.Parameters.AddWithValue("@NativeAddress", obj.NativeAddress);
+            com.Parameters.AddWithValue("@NativeDistrictID", obj.NativeDistrictID);
+            com.Parameters.AddWithValue("@NativeTalukaID", obj.NativeTalukaID);
+            com.Parameters.AddWithValue("@NativePinCode", obj.NativePINCODE);
+            com.Parameters.AddWithValue("@PhoneNumber", obj.PhoneNumber);
+            com.Parameters.AddWithValue("@AlternatePhoneNumber", obj.AlternatePhoneNumber);
+            com.Parameters.AddWithValue("@EmergencyContactNumber", obj.EmergencyContactNumber);
+            com.Parameters.AddWithValue("@EmergencyContactName", obj.EmergencyContactName);
+            com.Parameters.AddWithValue("@DateOfBirth", obj.DateOfBirth);
+            com.Parameters.AddWithValue("@Dateofjoin", obj.Dateofjoin);
+            com.Parameters.AddWithValue("@DateofRetirement", obj.DateofRetirement);
+            com.Parameters.AddWithValue("@DistrictJoiningDate", obj.DistrictJoiningDate);
+            com.Parameters.AddWithValue("@EmployeeEntryinDistrict", Convert.ToString(obj.EmployeeEntryinDistrict));
+            com.Parameters.AddWithValue("@EmployeeType", obj.EmployeeType);
+            com.Parameters.AddWithValue("@AgencyID", obj.AgencyID);
+            com.Parameters.AddWithValue("@CreatedBy", obj.CreatedBy);
+            com.Parameters.AddWithValue("@CreatedDate", DateTime.Now);
+            com.Parameters.AddWithValue("@UpdatedBy", obj.UpdatedBy);
+            com.Parameters.AddWithValue("@UpdatedDate", DateTime.Now);
+            com.Parameters.AddWithValue("@isActive", 1);
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<District> GetAllDistrict()
+        {
+            List<District> Districts = new List<District>();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("GetAllDistricts", con);
+            com.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataReader read = com.ExecuteReader();
+            while (read.Read())
+            {
+                Districts.Add(new District
+                { 
+                    DistrictID = Convert.ToInt32(read["DistrictID"]),
+                    DistrictName = Convert.ToString(read["DistrictName"])
+                }); 
+            }
+
+            return Districts;
+        }
+
+        public List<Taluka> GetAllTaluka(int DistrictID)
+        {
+            List<Taluka> Talukas = new List<Taluka>();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("GetAllTaluka", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@DistrictID", DistrictID);
+            DataTable dt = new DataTable();
+            SqlDataReader read = com.ExecuteReader();
+            while (read.Read())
+            {
+                
+                Talukas.Add(new Taluka
+                {
+                        TalukaID = Convert.ToInt32(read["TalukaID"]),
+                        TalukaName =Convert.ToString(read["TalukaName"])
+                });
+            }
+            return Talukas;
+
+        }
+
+        public List<string> GetAllAgency()
+        {
+            List<string> Agencies = new List<string>();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("GetAllAgencies", con);
+            com.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataReader read = com.ExecuteReader();
+            while (read.Read())
+            {
+                Agencies.Add(Convert.ToString(read["AgencyName"]));
+            }
+            return Agencies;
+
+        }
+        public List<Taluka> AllTalukasDetail()
+        {
+            List<Taluka> Talukaslist = new List<Taluka>();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+
+                using (SqlCommand cmd = new SqlCommand("AllTalukaDetail"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            Talukaslist.Add(new Taluka
+                            {
+                                DistrictID = Convert.ToInt32(sdr["DistrictID"]),
+                                TalukaID = Convert.ToInt32(sdr["TalukaID"]),
+                                TalukaName = Convert.ToString(sdr["TalukaName"]),
+                                CreatedBy = Convert.ToInt32(sdr["CreatedBy"]),
+                                CreatedDate = Convert.ToDateTime(sdr["CreatedDate"])
+                            });
+                        }
+                        con.Close();
+                        return Talukaslist;
+                    }
+                }
+            }
+        }
+
+        public List<District> AllDistricsDetail()
+        {
+            List<District> Districtslist = new List<District>();
+            string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+
+                using (SqlCommand cmd = new SqlCommand("DetailsDistrict"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            Districtslist.Add(new District()
+                            {
+                                DistrictID = Convert.ToInt32(sdr["DistrictID"]),
+                                DistrictName = Convert.ToString(sdr["DistrictName"]),
+                                CreatedBy = Convert.ToInt32(sdr["CreatedBy"]),
+                                CreatedDate = Convert.ToDateTime(sdr["CreatedDate"])
+                            });
+                        }
+                        con.Close();
+                        return Districtslist;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+    /* public bool deleteEmployee(int EmployeeId)
+         {
+             string constr = ConfigurationManager.ConnectionStrings["EmpConnect"].ConnectionString;
+             SqlConnection con = new SqlConnection(constr);
+             con.Open();
+             SqlCommand com = new SqlCommand("DeleteEmployee", con);
+             com.CommandType = CommandType.StoredProcedure;
+             com.Parameters.AddWithValue("@EmployeeID", EmployeeId);
+
+             int i = com.ExecuteNonQuery();
+             con.Close();
+             if (i >= 1)
+             {
+                 return true;
+             }
+             else
+             {
+                 return false;
+             }
+
+
+         }
+
+     }*/
+
